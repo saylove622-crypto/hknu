77e34586c60059694bf0de82c5c5ea824a6fd60d
saylove622-crypto <saylove622@gmail.com>
2026-06-07 02:25:53 +0900

    Updates

--- _next/static/QIuUpM0YsjUb_N65Sk9Tf/_buildManifest.js
+++ _next/static/QIuUpM0YsjUb_N65Sk9Tf/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/hknu//_next/:path+",
-        "destination": "/hknu/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
